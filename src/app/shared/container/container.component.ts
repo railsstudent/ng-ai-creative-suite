cc1a32659dd004ebb71de1f63944a7cd84429267
Connie Leung <cleung334@gmail.com>
2025-10-11 08:52:26 +0800

    fix: select content by attribute name

--- src/app/shared/container/container.component.ts
+++ src/app/shared/container/container.component.ts
@@ -4,8 +4,8 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   selector: 'app-container',
   template: `
     <div class="container">
-      <h1 class="main-header"><ng-content [select]="header1" /></h1>
-      <p class="main-header2"><ng-content [select]="header2" /></p>
+      <h1 class="main-header"><ng-content select="[header1]" /></h1>
+      <p class="main-header2"><ng-content select="[header2]" /></p>
       <ng-content />
     </div>
   `,
